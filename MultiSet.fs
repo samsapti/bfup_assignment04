abf4b19d288c126cf8ac39ba551b270790e4be3b
salsitu <sals@itu.dk>
2022-02-23 12:46:36 +0100

    Exercise 1 (green)

--- MultiSet.fs
+++ MultiSet.fs
@@ -1,2 +1,31 @@
 module MultiSet
 
+type MultiSet<'a when 'a: comparison> = S of Map<'a, uint32>
+
+let empty = S(Map.empty)
+
+let size (S (s)) =
+    Map.fold (fun acc _ value -> acc + value) 0u s
+
+let isEmpty s = (size s) = 0u
+let contains a (S (s)) = Map.containsKey a s
+
+let numItems a (S (s)) =
+    Map.tryFind a s |> Option.defaultValue 0u
+
+let add a n (S (s)) =
+    S(Map.add a ((numItems a (S(s))) + n) s)
+
+let addSingle a s = add a 1u s
+
+let remove a n (S (s)) =
+    let sz = numItems a (S(s))
+
+    if n < sz then
+        S(Map.add a (sz - n) s)
+    else
+        S(Map.remove a s)
+
+let removeSingle a s = remove a 1u s
+let fold f acc (S (s)) = Map.fold f acc s
+let foldBack f (S (s)) acc = Map.foldBack f s acc
--- MultiSet.fsi
+++ MultiSet.fsi
@@ -1,6 +1,14 @@
 module MultiSet
 
-// Map<'a, uint32> when 'a : comparison
-
-type MultiSet
-    val empty
+type MultiSet<'a when 'a: comparison>
+val empty : MultiSet<'a>
+val size : MultiSet<'a> -> uint32
+val isEmpty : MultiSet<'a> -> bool
+val contains : 'a -> MultiSet<'a> -> bool
+val numItems : 'a -> MultiSet<'a> -> uint32
+val add : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a>
+val addSingle : 'a -> MultiSet<'a> -> MultiSet<'a>
+val remove : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a>
+val removeSingle : 'a -> MultiSet<'a> -> MultiSet<'a>
+val fold : ('a -> 'b -> uint32 -> 'a) -> 'a -> MultiSet<'b> -> 'a
+val foldBack : ('a -> uint32 -> 'b -> 'b) -> MultiSet<'a> -> 'b -> 'b
